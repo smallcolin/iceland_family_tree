e6ead0d386154ad726e44598353e3cea6eebe741
smallcolin <colinsweeney2000@gmail.com>
2017-06-03 15:50:25 +0200

    404 page added

--- 404.php
+++ 404.php
@@ -1,6 +1,19 @@
 <?php get_header(); ?>
 
-	<h1>Whoops!</h1>
-	<p>Nah, somehow you've gone wrong.  </p>
+	<section class="fournollfour">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-sm-offset-3">
+					<h1>Whoops!</h1>
+					<p>Nah, somehow you've gone wrong.  </p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 aligncenter">
+					<button class="btn btn-danger">Back</button>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
