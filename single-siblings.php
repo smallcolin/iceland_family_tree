c63e2c7c0e2bf6c3f450d205610a29f079a9dae6
smallcolin <colinsweeney2000@gmail.com>
2017-06-04 14:22:54 +0200

    background color change and some minor code adjustments

--- single-siblings.php
+++ single-siblings.php
@@ -1,129 +1,98 @@
 <?php get_header();
 
-  if (have_posts()) :
-    while (have_posts()) : the_post();
-
       $name = get_field('name');
       $image = get_field('image');
       $dob = get_field('date_of_birth');
-
       $addy_1 = get_field('address_1');
       $addy_2 = get_field('address_2');
       $postcode = get_field('postcode');
       $country = get_field('country');
       $job = get_field('job');
-
       $parent = get_field('parent');
-      // $child = get_sub_field('child', $post_id);
-      $child = get_field('children'); // ACF Repeater
-      $grandchild = get_sub_field('grandchild');
-      $partner = get_sub_field('partner');
-
     ?>
-      <section class="single-person">
-
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12 col-sm-6">
-              <?php
+    <section class="single-person">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-6"> <!--Person Details-->
+            <?php
               if ($image) {
                 echo '<img src="' . $image . '" class="person-image" />';
               }
-              ?>
-              <table class="person-details">
-                <tbody>
-                  <tr>
-                    <?php
-                      if ( $name ) {
-                        echo '<td>Name: </td> ' . '<td><a href="#">' . $name . '</a></td>';
-                      }
+            ?>
+            <table class="person-details">
+              <tbody>
+                <tr>
+                  <td><h3>Name</h3></td>
+                  <td><h3><?php if ( $name ) { echo $name; } ?></h3></td>
+                </tr>
+                <tr>
+                  <td>Date of Birth</td>
+                  <td><?php if ( $dob ) { echo $dob; } ?></td>
+                </tr>
+                <tr>
+                  <td>Address</td>
+                  <td><?php if ( $addy_1 ) { echo $addy_1; } ?></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td><?php if ( $addy_2 ) { echo $addy_2; } ?></td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td><?php if ( $postcode ) { echo $postcode; } ?></td>
+                </tr>
+                <tr>
+                  <td>Country</td>
+                  <td><?php if ( $country ) { echo $country; } ?></td>
+                </tr>
+                <tr>
+                  <td>Occupation</td>
+                  <td><?php if ( $job ) { echo $job; } ?></td>
+                </tr>
+                <tr>
+                  <td>Partner</td>
+                  <td>
+                    <?php if (get_field('partner')) { ?>
+                      <a href="<?php the_permalink(get_field('partner')); ?>"><?php echo get_the_title(get_field('partner')); ?></a>
+                    <?php }
                     ?>
-                  </tr>
-                  <tr>
-                    <?php
-                      if ( $dob ) {
-                        echo '<td>Date of Birth: </td>' . '<td>' . $dob . '</td>';
-                      }
-                    ?>
-                  </tr>
-                  <tr>
-                    <td>Address</td>
-                    <?php
-                      if ( $addy_1 ) {
-                        echo '<td>' . $addy_1 . '</td>';
-                      }
-                    ?>
-                  </tr>
-                  <tr>
-                    <td></td>
-                    <?php
-                      if ( $addy_2 ) {
-                        echo '<td>' . $addy_2 . '</td>';
-                      }
-                    ?>
-                  </tr>
-                  <tr>
-                    <td></td>
-                      <?php
-                        if ( $postcode ) {
-                          echo '<td>' . $postcode . '</td>';
-                        }
-                      ?>
-                  </tr>
-                  <tr>
-                    <?php
-                      if ( $country ) {
-                        echo '<td>Country: </td>' . '<td>' . $country . '</td>';
-                      }
-                    ?>
-                  </tr>
-                  <tr>
-                    <?php
-                      if ( $job ) {
-                        echo '<td>Occupation: </td>' . '<td>' . $job . '</td>';
-                      }
-                    ?>
-                  </tr>
-                </tbody>
-              </table>
-
-            </div>
-
-            <div class="col-xs-12 col-sm-6">  <!-- Family members -->
-              <?php if ( $parent ) {
-              echo $parent;
-            }
-            // CHILDREN
-            if(have_rows('children')) :
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-xs-12 col-sm-6 aligncenter">  <!-- Family members -->
+            <h2>Family</h2>
+            <!-- SIBLINGS -->
+            <h3>Siblings</h3>
+            <?php if (have_rows('siblings')) :
+              while (have_rows('siblings')) : the_row();
+                if ( get_sub_field('sibling') ) { ?>
+                  <a href="<?php the_permalink(get_sub_field('sibling')); ?>"><?php echo get_the_title(get_sub_field('sibling')); ?></a>
+                <?php }
+              endwhile;
+            endif;
+            ?>
+            <!-- CHILDREN -->
+            <h3>Children</h3>
+            <?php if(have_rows('children')) :
               while(have_rows('children')) : the_row(); ?>
-                <!-- <pre><?php //var_dump($child); ?></pre> -->
                 <?php if ( get_sub_field('child') ) { ?>
-                  <a href="<?php the_permalink(get_sub_field('child')); ?>"><?php the_sub_field('child', $post_object->ID); ?></a>
-
+                  <a href="<?php the_permalink(get_sub_field('child')); ?>"><?php echo get_the_title(get_sub_field('child')); ?></a>
                 <?php }
               endwhile;
             endif; ?>
-
-            <!-- CHILDREN II -->
-            <p>Test</p>
-            <?php foreach ($child as $children) {
-              // echo '<p>' . $children['child'] . '</p>';
-            } ?>
-
-             <!-- GRANDCHILDREN -->
-
-            <!-- Need to test $child & $grandchild & $partner.  Need to be links -->
-
+            <!-- GRANDCHILDREN -->
+            <h3>GrandChildren</h3>
+            <?php if(have_rows('grandchildren')) :
+              while(have_rows('grandchildren')) : the_row(); ?>
+                <?php if ( get_sub_field('grandchild') ) { ?>
+                  <a href="<?php the_permalink(get_sub_field('grandchild')); ?>"><?php echo get_the_title(get_sub_field('grandchild')); ?></a>
+                <?php }
+              endwhile;
+            endif; ?>
           </div>
         </div>
       </div>
     </section>
-
-
-
-
-
-    <?php endwhile;
-  endif;
-
-get_footer(); ?>
+<?php get_footer(); ?>
