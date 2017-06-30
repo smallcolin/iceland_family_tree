4cd5c0587dcca6f7117cdef03ecf6f0703b16c4f
smallcolin <colinsweeney2000@gmail.com>
2017-06-30 23:02:55 +0000

    changed message

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
   if (have_posts()) :
     while (have_posts()) : the_post();
       the_title();
-      echo '<h2>This is yer single.php';
+      echo '<h2>Individual persons should be found here but something has gone wrong</h2>';
 
     endwhile;
   endif;
