28a610869a84c3b6ba6f2d78cebc52fa0812c626
smallcolin <colinsweeney2000@gmail.com>
2017-07-02 01:47:17 +0000

    fixed taxonomy pages

--- taxonomy-country.php
+++ taxonomy-country.php
@@ -4,29 +4,31 @@
     <div class="container">
       <div class="row">
         <div class="col-xs-12 aligncenter">
+          <h2><?php single_term_title("Country of residence: "); ?></h2>
+          <p><?php echo get_terms('country'); ?></p>
+          <pre><?php var_dump(get_terms('country')); ?></pre>
         </div>
       </div>
       <div class="row">
-        <h2>Country of residence: <?php single_term_title(); ?></h2>
         <?php
-          if (have_posts('partners')) :
-            while (have_posts('partners')) : the_post();
-            $partner = get_sub_field('partner');
+          if (have_posts()) :
+            while (have_posts()) : the_post();
+            $child = get_sub_field('child');
             $image = get_field( 'image' );
           ?>
             <div class="col-xs-6 col-sm-4 col-md-3 aligncenter">
               <h3>
-                <a href="<?php the_permalink($partner); ?>">
+                <a href="<?php the_permalink($child); ?>">
                   <?php if ( $image ) { ?>
                     <div class="thumbs" style="background-image: url('<?php echo $image; ?>');">
                       <!-- Mobile Screen -->
                       <div class="mobile-title visible-xs">
-                        <?php the_title($partner) ?>
+                        <?php the_title($child) ?>
                       </div>
                       <!-- Full screen title -->
                       <div class="filter hidden-xs hidden-sm hidden-md">
                         <h2 class="hidden-title">
-                          <?php the_title($partner); ?>
+                          <?php the_title($child); ?>
                         </h2>
                       </div>
                     </div>
