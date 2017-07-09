7a1c0d0b4a571053b4b183727d148174ffd7dede
smallcolin <colinsweeney2000@gmail.com>
2017-07-09 23:04:56 +0000

    bootstrap alignment error

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 	<header id="header">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-8 col-sm-4">
+				<div class="col-xs-10 col-sm-4">
 					<a href="<?php bloginfo("url"); ?>">
 						<h2><?php bloginfo('site_name'); ?></h2>
 					</a>
@@ -23,7 +23,7 @@
 					<?php wp_nav_menu(); ?>
 				</div>
 				<nav>
-					<div class="col-xs-3 col-sm-8 hidden-md hidden-lg menu-icon">
+					<div class="col-xs-2 col-sm-8 hidden-md hidden-lg menu-icon">
 						<i class="fa fa-bars fa-2x"></i>
 					</div>
 					<div class="responsive-menu">
