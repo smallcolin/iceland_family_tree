8735d8379abb27da8b968d6ca3ece5b064dd984e
smallcolin <colinsweeney2000@gmail.com>
2017-07-13 18:22:46 +0000

    finalised splash page

---
+++ header-alt.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<title>
+		<?php wp_title(); ?>
+	</title>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
--- templates/splash.php
+++ templates/splash.php
@@ -1,32 +1,24 @@
-<?php 
-
+<?php
 /* Template name: Splash Page */
+get_header('alt');
+?>
 
-//get_header(); ?>
+	<section class="splash">
+		<?php
+			$image = get_field('splash_image', 'options');
+			$title = get_field('splash_title', 'options');
+			if ( $image ) : ?>
+				<a href="<?php echo get_permalink( get_option('page_for_posts')); ?>">
+					<div class="front-image" style="background-image: url('<?php echo $image; ?>');">
+						<h1 class="frontpage-title"><?php echo $title; ?></h1>
+					</div>
+				</a>
+			<?php else : ?>
+				<a href="<?php echo get_permalink( get_option('page_for_posts')); ?>">
+					<h1 class="frontpage-title"><?php echo $title; ?></h1>
+				</a>
+			<?php endif;
+		?>
+	</section>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 aligncenter">
-
-				<?php if ( have_posts() ) :
-
-					while ( have_posts() ) : the_post(); ?>
-
-						<a href="<?php echo get_permalink( get_option('page_for_posts')) ?>">
-							<div style="background-image: url('<?php echo get_template_directory_uri().'/assets/img/akureyri_winter.jpg'; ?>'); height: 100%; background-repeat: no-repeat; background-size: cover;"></div>
-						</a>
-						
-
-					<?php endwhile; ?>
-
-					<?php else : ?>
-
-						<?php get_template_part( 'partials/content', 'none' ) ?>
-
-				<?php endif; ?>
-	
-			</div>
-		</div>
-	</div>
-
-<?php //get_footer(); ?>
+<?php get_footer('alt');
