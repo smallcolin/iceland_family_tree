e94157435a392c81eed8e739697d4e542e425137
smallcolin <colinsweeney2000@gmail.com>
2017-07-23 15:30:57 +0200

    added php date function to keep footer updated

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 		</main>
-		
+
 		<footer id="footer">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-xs-12">
-						<p class="aligncenter">&copy; Colin Sweeney 2016</p>
+						<p class="aligncenter">&copy; Colin Sweeney <?php echo date('Y'); ?></p>
 					</div>
 				</div>
 			</div>
-			
+
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
