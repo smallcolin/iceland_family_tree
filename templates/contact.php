a9f2e0796c263987fa70e4aaccdca17051276785
smallcolin <colinsweeney2000@gmail.com>
2017-08-08 16:24:47 +0200

    further css changes to allow for huge spaces when doing a search

--- templates/contact.php
+++ templates/contact.php
@@ -1,30 +1,24 @@
-<?php 
+<?php
 
 /* Template name: Contact Page */
 
 get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 aligncenter">
-
-				<?php if ( have_posts() ) :
-
-					while ( have_posts() ) : the_post(); ?>
-
-						<h3><?php the_title(); ?></h3>
-						<?php the_content(); ?>
-
-					<?php endwhile; ?>
-
+	<section class="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 aligncenter">
+					<?php if ( have_posts() ) :
+						while ( have_posts() ) : the_post(); ?>
+							<h3><?php the_title(); ?></h3>
+							<?php the_content(); ?>
+						<?php endwhile; ?>
 					<?php else : ?>
-
 						<?php get_template_part( 'partials/content', 'none' ) ?>
-
-				<?php endif; ?>
-	
+					<?php endif; ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 
 <?php get_footer(); ?>
