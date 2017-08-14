1ae81ce6e8a76aac27c44e33b70e99e7f070dd51
smallcolin <colinsweeney2000@gmail.com>
2017-08-14 13:14:45 +0200

    Edit to functions for DRY

--- includes/custom.php
+++ includes/custom.php
@@ -23,6 +23,87 @@ function cpt_taxonomy() {
   }
 }
 
+// Function for single person details
+
+  function person_details() {
+      $name = get_field('name');
+      $image = get_field('image');
+      $dob = get_field('date_of_birth');
+      $addy_1 = get_field('address_1');
+      $addy_2 = get_field('address_2');
+      $postcode = get_field('postcode');
+      $job = get_field('job');
+      $partner = get_field('child_partner');
+      $partImage = get_field('image', $partner->ID);
+    ?>
+    <div class="col-xs-12 col-sm-6"> <!--Person Details-->
+      <?php
+        if ($image) { ?>
+          <div class="thumbs person-image" style="background-image: url('<?php echo $image; ?>');"></div>
+        <?php } else { ?>
+          <div class="thumbs person-image"></div>
+        <?php }
+      ?>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <table class="person-details">
+        <tbody>
+          <tr>
+            <td><h3>Name</h3></td>
+            <td class="right"><h3><?php if ( $name ) { echo $name; } ?></h3></td>
+          </tr>
+          <tr>
+            <?php if ($dob) { ?>
+              <td>Date of Birth</td>
+              <td class="right"><?php echo $dob; ?></td>
+            <?php } ?>
+          </tr>
+          <tr>
+            <?php if ($addy_1 || $addy_2) { ?>
+              <td>Address</td>
+            <?php } ?>
+            <td class="right"><?php if ( $addy_1 ) { echo $addy_1; } ?></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="right"><?php if ( $addy_2 ) { echo $addy_2; } ?></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="right"><?php if ( $postcode ) { echo $postcode; } ?></td>
+          </tr>
+          <tr>
+            <td>Lives</td>
+            <td class="right"><?php echo cpt_taxonomy(); ?></td>
+          </tr>
+          <tr>
+            <?php if ( $job ) { ?>
+              <td>Occupation</td>
+              <td class="right"><?php echo $job; ?></td>
+            <?php } ?>
+          </tr>
+          <tr>
+            <?php if ($partner) { ?>
+              <td>Partner</td>
+              <td class="right">
+                <a class="partner-link" href="<?php the_permalink($partner); ?>">
+                  <?php echo get_the_title($partner); ?>
+                </a>
+              </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="right">
+              <img class="partner-image" src="<?php echo $partImage; ?>" alt="partner">
+            </td>
+            <?php } ?>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  <?php }
+
+
 // Pagination stuff
 
   function paginator(){
--- single-grandchildren.php
+++ single-grandchildren.php
@@ -14,66 +14,8 @@
 ?>
   <section class="single-person">
     <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-6"> <!--Person Details-->
-          <?php
-            if ($image) { ?>
-              <div class="thumbs person-image" style="background-image: url('<?php echo $image; ?>');"></div>
-            <?php } else { ?>
-              <div class="thumbs person-image"></div>
-            <?php }
-          ?>
-        </div>
-        <div class="col-xs-12- col-sm-6">
-          <table class="person-details">
-            <tbody>
-              <tr>
-                <td><h3>Name</h3></td>
-                <td class="right"><h3><?php if ( $name ) { echo $name; } ?></h3></td>
-              </tr>
-              <tr>
-                <?php if ($dob) { ?>
-                  <td>Date of Birth</td>
-                  <td class="right"><?php echo $dob; ?></td>
-                <?php } ?>
-              </tr>
-              <tr>
-                <td>Address</td>
-                <td class="right"><?php if ( $addy_1 ) { echo $addy_1; } ?></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td class="right"><?php if ( $addy_2 ) { echo $addy_2; } ?></td>
-              </tr>
-              <tr>
-                <td></td>
-                <td class="right"><?php if ( $postcode ) { echo $postcode; } ?></td>
-              </tr>
-              <tr>
-                <td>Lives</td>
-                <td class="right"><?php echo cpt_taxonomy(); ?></td>
-              </tr>
-              <tr>
-                <?php if ( $job ) { ?>
-                  <td>Occupation</td>
-                  <td class="right"><?php echo $job; ?></td>
-                <?php } ?>
-              </tr>
-              <tr>
-                <?php if ($partner) { ?>
-                  <td>Partner</td>
-                  <td class="right">
-                    <img class="partner-image" src="<?php echo $partImage; ?>" alt="partner">
-                    <a class="partner-link" href="<?php the_permalink($partner); ?>">
-                      <?php echo get_the_title($partner); ?>
-                    </a>
-                    <?php }
-                    ?>
-                  </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div class="row"> <!--Person Details-->
+        <?php person_details(); ?>
       </div>
       <div class="row"> <!-- Family members -->
         <div class="col-xs-12 col-sm-4 aligncenter">
