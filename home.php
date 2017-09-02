a5e9df35459a9a926362c0ce1c287400fc562c66
smallcolin <colinsweeney2000@gmail.com>
2017-09-02 20:13:14 +0200

    added original parents section

--- home.php
+++ home.php
@@ -1,6 +1,33 @@
-<?php get_header();
+<?php
+  $grandmother = get_field('grandmother_image', 'options' );
+  $grandmother_name = get_field('grandmother_name', 'options');
+  $grandfather = get_field('grandfather_image', 'options');
+  $grandfather_name = get_field('grandfather_name', 'options');
 
-  if( have_posts() ) :
+get_header(); ?>
+
+  <section class="originals">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6">
+          <div class="parents" style="background-image: url('<?php echo $grandmother; ?>');">
+          </div>
+          <h3>
+            <?php echo $grandmother_name; ?>
+          </h3>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+          <div class="parents" style="background-image: url('<?php echo $grandfather; ?>');">
+          </div>
+          <h3>
+            <?php echo $grandfather_name; ?>
+          </h3>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <?php if( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
 
       <section class="page-top">
