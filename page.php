2285c4d4b74e9d353eed939d868f76458145ec65
smallcolin <colinsweeney2000@gmail.com>
2017-09-03 02:01:52 +0200

    reformatted page.php

--- page.php
+++ page.php
@@ -1,42 +1,14 @@
-<?php get_header(); 
+<?php get_header(); ?>
 
-  if( have_posts() ) : 
-    while ( have_posts() ) : the_post(); ?>
-
-      <section class="page-top">
+      <section class="family">
         <div class="container">
           <div class="row">
-            <div class="col-xs-12">
-              <?php the_title(); ?>
-            </div>
+            <?php
+              the_title();
+              the_content();
+            ?>
           </div>
         </div>
       </section>
 
-      <section class="page-middle">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <?php the_content(); ?>
-              PAGE CONTEXT
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="page-bottom">
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-12">
-              <?php the_post_thumbnail(); ?>
-              PAGE BOTTOM CONTEXT WITH AN IMAGE
-              <div style="background-image: url('<?php echo get_template_directory_uri().'/assets/img/akureyri_winter.jpg'; ?>'); height: 500px;"></div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-    <?php endwhile;
-  endif;
-
-get_footer(); ?>
+<?php get_footer(); ?>
