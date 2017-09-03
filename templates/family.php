63eacc21f8e02329a06e6f0efccf47add2889db3
smallcolin <colinsweeney2000@gmail.com>
2017-09-03 02:04:53 +0200

    further family files

---
+++ templates/family.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Template name: Family Page */
+
+get_header(); ?>
+
+      <section class="family">
+        <div class="container">
+          <div class="row">
+            <?php
+              show_cpt_menu();
+            ?>
+          </div>
+        </div>
+      </section>
+
+<?php get_footer(); ?>
