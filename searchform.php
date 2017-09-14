6facd9ae94ac42335a7180e7b0bec92fadeaf7e0
smallcolin <colinsweeney2000@gmail.com>
2017-09-14 13:02:36 +0200

    syntax error

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
 ?>
 <form class="searchform aligncenter" action="<?php bloginfo("url");?>" method="get" role="search">
   <div class="inner-search">
-    <input type="text" name="s" id="search-input" placeholder=" ex. Eyrun, Idunn, etc" class="text-input" value="<?php echo $value ?>" autofocus />
+    <input type="text" name="s" id="search-input" placeholder=" ex. Eyrun, Idunn, etc" class="text-input" value="<?php echo $value; ?>" autofocus />
     <input class="btn btn-danger button detail submit-search" type="submit" value="Search" />
   </div>
 </form>
