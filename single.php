de6a76e680638cfe6584344058626dc0f9a627e5
smallcolin <colinsweeney2000@gmail.com>
2017-12-19 15:29:09 +0100

    added more complete layout to basic wp posts page

--- single.php
+++ single.php
@@ -1,11 +1,20 @@
 <?php get_header();
 
-  if (have_posts()) :
-    while (have_posts()) : the_post();
-      the_title();
-      echo '<h2>Individual persons should be found here but something has gone wrong</h2>';
 
-    endwhile;
+  if (have_posts()) :
+    while (have_posts()) : the_post(); ?>
+
+    <section class="single-person">
+      <div class="container">
+        <h3>
+          <?php the_title(); ?>
+        </h3>
+        <p>
+          <?php the_content(); ?>
+        </p>
+      </div>
+    </section>
+    <?php endwhile;
   endif;
 
 get_footer(); ?>
