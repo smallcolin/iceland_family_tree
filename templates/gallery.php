cab93a0e4a22035aeb69344773df8bde56c2ca42
smallcolin <colinsweeney2000@gmail.com>
2017-12-25 20:30:20 +0100

    Translated page name

--- templates/gallery.php
+++ templates/gallery.php
@@ -7,7 +7,7 @@ get_header(); ?>
   <section class="gallery">
     <div class="container">
       <div class="row aligncenter">
-        <h2>Gallery</h2>
+        <h2>Myndir</h2>
         <div class="col-xs-12 col-sm-8 col-sm-offset-2">
           <?php
             one_more_gallery();
